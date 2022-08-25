a89880f0432f115d9856497dd63fbbb52871c71c
Robin Joseph <robinjosephiit@gmail.com>
2022-08-25 10:53:11 +0530

    Updated to show real time U mean and rms

--- Acquisition/Hotwire_Check_Signal.py
+++ Acquisition/Hotwire_Check_Signal.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Jun  6 19:31:43 2022
 
-@author: Sai
+@author: Robin
 """
 
 #############Checking signals###########################
@@ -16,23 +16,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 ###############################################################################
 #Inputs
-SR = 20000 #Sampling rate
+paramet=np.loadtxt('D:\Robin_24th_Aug\Calibration_1130am_25.02_deg\Constants.dat')
+nn=paramet[0]
+aa=paramet[1]
+bb=paramet[2]
+Uinf=2.88 # Make sure to correct Uinf here
+
+
+SR = 40000 #Sampling rate
 n = SR #Number of samples to read
+E = np.zeros(n) #Creating an array to store samples
 v = np.zeros(n) #Creating an array to store samples
 ###############################################################################
 #Creating a loop to see realtime signal
 plt.figure(1)
 while True:
     task = nidaqmx.Task()
-    task.ai_channels.add_ai_voltage_chan("Dev2/ai2")
+    task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
     task.timing.cfg_samp_clk_timing(SR, source='', active_edge=Edge.RISING, sample_mode=AcquisitionType.FINITE, samps_per_chan=n)
     task.start()
-    v = task.read(number_of_samples_per_channel=n,timeout = 60)
+    E = task.read(number_of_samples_per_channel=n,timeout = 60)
+    for i in range(len(E)):
+        v[i]=(((E[i]**2)-aa)/bb)**(1/nn)
     plt.plot(v, linewidth=0.4)
+    temp=np.round((np.mean(v)),2)
+    temp=str('U=')+str(temp)+str('  urms/U=')+str(np.round(np.std(v-np.mean(v))/Uinf,3))
+    
+    plt.legend([temp])
     plt.show()
     plt.grid(True)
-    # plt.ylim([1.4, 1.75])
+    plt.ylim([2, 4])
     task.stop
     task.close()
+    
 ###############################################################################
 print('Completed')
+
+
+
+    
+
+
+
+
+
